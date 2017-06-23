965c014060b6a68686240be3e34e29dea031a148
SREELAKSHMY SHAJAN <04414@user>
2017-06-23 10:44:35 -0400

    Sorting an array using insertion sort

---
+++ insertion.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+ 
+int main()
+{
+int array[7]={4,23,43,38,15,10};
+int i,j,k;
+for(i=1;i<7;i++)
+{
+  k=array[i];
+  for(j=i-1;j>=0&k<array[j];j--)
+   {
+     array[j+1]=array[j];
+
+     array[j]=k;
+   }
+}
+ printf("The Sorted array:\n");
+
+for(i=0;i<7;i++)
+{
+printf("%d  ",array[i]);
+}
+printf("\n");
+}
+
