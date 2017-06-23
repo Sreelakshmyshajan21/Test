0d425a58b6d35145d3a0c496535393dce923dc4f
SREELAKSHMY SHAJAN <04414@user>
2017-06-23 11:02:33 -0400

    inserting function

--- insertion.c
+++ insertion.c
@@ -1,8 +1,17 @@
 #include<stdio.h>
-int i,j,k;
+int i,j,k,n,array[50];
+int insert()
+{
+printf("enter the limit\n");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&array[i]);
+}
+}
 int sort(int *array)
 {
-for(i=1;i<7;i++)
+for(i=1;i<n;i++)
 {
   k=array[i];
   for(j=i-1;j>=0&k<array[j];j--)
@@ -16,12 +25,12 @@ return 0;
 } 
 int main()
 {
-int array[7]={4,23,43,38,15,10};
+//int array[7]={4,23,43,38,15,10};
 
 sort(array);
  printf("The Sorted array:\n");
 
-for(i=0;i<7;i++)
+for(i=0;i<n;i++)
 {
 printf("%d  ",array[i]);
 }
